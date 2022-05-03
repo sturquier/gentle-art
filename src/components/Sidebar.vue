272c00c55082190905dba29ed02f25d8ee4acfc1
sturquier <turquiersimon@hotmail.fr>
2022-05-03 16:55:00 +0200

    Improve sidebar rendering with routes groups toggle

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
 import { routes, Route } from '../router'
 
 const routesHavingChildren: Array<Route> = routes.filter(route => route.children)
+
+const openedGroup: Ref<number> = ref(0)
+const toggleGroup = (index: number) => openedGroup.value === index ? openedGroup.value = null : openedGroup.value = index
 </script>
 
 <template>
   <v-navigation-drawer>
-    <v-list>
+    <v-list class="routes">
       <v-list-item
         v-for="(route, i) in routesHavingChildren"
         :key="i"
       >
-        <v-list-item-subtitle>{{ route.label }}</v-list-item-subtitle>
-        <v-list>
+        <v-list-item-subtitle
+          class="label"
+          @click="toggleGroup(i)"
+        >
+          {{ route.label }}
+        </v-list-item-subtitle>
+        <v-icon
+          size="x-small"
+          :icon="i === openedGroup ? 'mdi-arrow-up' : 'mdi-arrow-down'"
+          class="arrow"
+          @click="toggleGroup(i)"
+        ></v-icon>
+        <v-list v-if="i === openedGroup">
           <v-list-item
             v-for="(child, j) in route.children"
             :key="j"
@@ -26,13 +43,26 @@ const routesHavingChildren: Array<Route> = routes.filter(route => route.children
             </router-link>
           </v-list-item>
         </v-list>
-        <v-divider></v-divider>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <style>
+  .routes {
+    margin-top: 15px;
+  }
+
+  .arrow {
+    float: right;
+    bottom: 16px;
+    opacity: 0.5;
+  }
+
+  .label, .arrow {
+    cursor: pointer;
+  }
+
   .link {
     font-size: 14px;
     color: var(--black);
